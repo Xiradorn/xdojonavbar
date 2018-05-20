c6126df0ac3578f0a32b964382b56bf0ee6d23f4
Xiradorn <xiradorn@gmail.com>
2018-05-20 23:23:14 +0200

    Migration schema build/think

--- migrations/install_xen_schema.php
+++ migrations/install_xen_schema.php
@@ -28,7 +28,49 @@ class install_xen_schema extends \phpbb\db\migration\migration
 		 * Add data for general config stuff
 		 */
 		return array(
-			array('config.add', array('xiradorn_xen', 1))
+			array('config.add', array('xiradorn_xen_enabled', 1))
+		);
+	}
+
+	public function update_schema()
+	{
+		/**
+		 * Add table for support menu
+		 */
+		return array(
+			'add_tables'	=> array(
+				$this->table_prefix . 'xiradorn_xen_config'	=> array(
+					'COLUMNS'	=> array(
+						'xen_config_id'					=> array('UINT', null, 'auto_increment'),
+						'xen_config_param'				=> array('VCHAR', ''),
+						'xen_config_value'				=> array('VCHAR', ''),
+						'xen_config_extra'				=> array('VCHAR', ''),
+					),
+					'PRIMARY_KEY'	=> array(
+						'xen_config_id',
+					),
+				),
+				$this->table_prefix . 'xiradorn_xen_items'	=> array(
+					'COLUMNS'	=> array(
+						'xen_item_id'					=> array('UINT', null, 'auto_increment'),
+						'xen_item_title'				=> array('TEXT', ''),
+						'xen_item_link'					=> array('TEXT', ''),
+						'xen_item_description'			=> array('TEXT', ''),
+						'xen_item_attributes'			=> array('TEXT', ''),
+						'xen_item_left_id'				=> array('UINT', 0),
+						'xen_item_right_id'				=> array('UINT', 0),
+						'xen_item_parent_id'			=> array('UINT', 0),
+						
+						'xen_subitem_enabled'			=> array('BOOL', 0),
+						'xen_subitem_type'				=> array('UINT', 0),
+						'xen_subitem_attributes'		=> array('TEXT', ''),
+						'xen_subitem_large_img_link'	=> array('TEXT', ''),
+					),
+					'PRIMARY_KEY'	=> array(
+						'xen_id',
+					),
+				),
+			),
 		);
 	}
 }
